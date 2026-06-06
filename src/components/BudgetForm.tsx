5bec91ddbbcea7f5f2a7fa86195fa5154bed45e0
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2026-06-06 01:01:43 -0600

    feat: connect BudgetForm to budget reducer via submit handler

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react"
-import type { ChangeEvent } from "react";
+import type { ChangeEvent, SubmitEvent } from "react";
+import { useBudget } from "../hooks/useBudget";
 
 export const BudgetForm = () => {
     const [budget, setBudget] = useState(0);
+    const { dispatch } = useBudget()
 
     const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
         // console.log([e.target.id], e.target.value)
@@ -11,8 +13,13 @@ export const BudgetForm = () => {
 
     const isValid = isNaN(budget) || budget <=0
 
+    const handleSubmit = (e:SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch({type: 'add-budget', payload: {budget}})
+    }
+
     return (
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={ handleSubmit }>
             <div className="flex flex-col space-y-5">
                 <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
                     Definir presupuesto
