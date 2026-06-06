5ef1cec98783895fca48dd0ab1f72a91875b5e9d
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2026-06-05 22:10:53 -0600

    feat: add BudgetForm component with budget input and validation

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import type { ChangeEvent } from "react";
+
+export const BudgetForm = () => {
+    const [budget, setBudget] = useState(0);
+
+    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+        // console.log([e.target.id], e.target.value)
+        setBudget(e.target.valueAsNumber)
+    }
+
+    const isValid = isNaN(budget) || budget <=0
+
+    return (
+        <form className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+                    Definir presupuesto
+                </label>
+                <input
+                    id="budget"
+                    type="number"
+                    className="w-full bg-white border-gray-200 p-2"
+                    placeholder="Define tu presupuesto"
+                    name="budget"
+                    value={budget}
+                    onChange={ handleChange }
+                />
+            </div>
+            <input 
+                type="submit"
+                value="Definir Presupuesto"
+                className="bg-blue-600 hover:bg-blue700 cursor-pointer w-full p-2 text-white font-black disabled:opacity-40"
+                disabled={isValid}     
+            />
+        </form>
+    )
+}
