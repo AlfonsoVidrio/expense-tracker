d39b4e26fb4fa63c57bdb996b0cad2398ad7cdbc
AlfonsoVidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2026-06-08 02:21:26 -0600

    feat: implement ExpenseForm with state, validation, and date picker

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,11 +1,63 @@
+import type { DraftExpense, Value } from "../types";
+import { useState, type ChangeEvent, type SubmitEvent } from "react";
 import { categories } from "../Data/categories"
+import DatePicker from 'react-date-picker';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import { ErrorMessage } from "./ErrorMessage";
+import { useBudget } from "../hooks/useBudget";
 
 export const ExpenseForm = () => {
+    const [expense, setExpense] = useState<DraftExpense>({
+        expenseName: '',
+        amount: 0,
+        category: '',
+        date: new Date()
+    })
+
+    const [error, setError] = useState('')
+    const { dispatch } = useBudget()
+
+    const handleChangeDate = (value: Value) => {
+        setExpense({
+            ...expense,
+            date: value
+        })
+    }
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = e.target
+        const isAmountField = ['amount'].includes(name)
+        setExpense({
+            ...expense,
+            [name]: isAmountField ? Number(value) : value
+        })
+    }
+
+    const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if (Object.values(expense).includes('') || expense.amount <= 0) {
+            setError("Todos los campos son obligatorios")
+            return
+        }
+        dispatch({ type: 'add-expense', payload: { expense } })
+
+        setExpense({
+            expenseName: '',
+            amount: 0,
+            category: '',
+            date: new Date()
+        })
+    }
+
     return (
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <legend
                 className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2"
             >Nuevo Gasto</legend>
+
+            {error && <ErrorMessage>{error}</ErrorMessage>}
 
             <div className="flex flex-col gap-2">
                 <label
@@ -19,6 +71,8 @@ export const ExpenseForm = () => {
                     placeholder="Añade el nombre del gasto"
                     className="bg-slate-100 p-2"
                     name="expenseName"
+                    value={expense.expenseName}
+                    onChange={handleChange}
                 />
             </div>
             <div className="flex flex-col gap-2">
@@ -33,6 +87,8 @@ export const ExpenseForm = () => {
                     placeholder="Añade la cantidad del gasto: ej. 300"
                     className="bg-slate-100 p-2"
                     name="amount"
+                    value={expense.amount}
+                    onChange={handleChange}
                 />
             </div>
             <div className="flex flex-col gap-2">
@@ -45,6 +101,8 @@ export const ExpenseForm = () => {
                     id="category"
                     className="bg-slate-100 p-2"
                     name="category"
+                    value={expense.category}
+                    onChange={handleChange}
                 >
                     <option value="">--- Seleccione ---</option>
                     {categories.map((category) => (
@@ -53,10 +111,23 @@ export const ExpenseForm = () => {
                 </select>
             </div>
 
+            <div className="flex flex-col gap-2">
+                <label
+                    htmlFor="amount"
+                    className="text-xl">
+                    Fecha Gasto:
+                </label>
+                <DatePicker
+                    className="bg-slate-100 p-2 border-0"
+                    value={expense.date}
+                    onChange={handleChangeDate}
+                />
+            </div>
+
             <input
-            type="submit" 
-            className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg" 
-            value={'Registrar Gasto'}
+                type="submit"
+                className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+                value={'Registrar Gasto'}
             />
 
         </form>
